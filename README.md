f0ab0963427fe3228909a5466c05fd62d26f2ac5
Victor Basumatary <vlenceleuth@gmail.com>
2020-11-16 11:25:08 +0530

    Add optimisation options

--- README.md
+++ README.md
@@ -4,7 +4,53 @@ Please do not use it for production.
 
 ## About
 
-A simple application that does image optimization on the fly. This application uses sharp to perform the optimizations and can do the following:
+A simple application that does image optimization on the fly. This can be useful, for example, in websites which contain unoptimised images and would be difficult to reupload optimised images. This application uses sharp to perform the optimizations and can do the following:
 
 - Change width and/or height of the image
 - Change format of the image to jpeg, png or webp
+- Apply optimisations specific to jpeg, png and webp
+
+## Options
+
+Pass format specfic options to apply optimisations. These options are 1:1 with sharp options.
+
+```
+/cat.png?ext=jpeg&options[quality]=1&options[progressive]=true
+```
+
+### jpeg
+
+- `quality`. Integer 1-100 (optional, default 80).
+- `progressive`. Use progressive (interlace) scan (optional, default false)
+- `chromaSubsampling`. Set to '4:4:4' to prevent chroma subsampling otherwise defaults to '4:2:0' chroma subsampling (optional, default '4:2:0').
+- `optimiseCoding` / `optimizeCoding`. Optimise Huffman coding tables (optional, default true)
+- `trellisQuantisation`. Apply trellis quantisation (optional, default false).
+- `overshootDeringing`. Apply overshoot deringing (optional, default false).
+- `optimiseScans` / `optimizeScans`. Optimise progressive scans, forces progressive (optional, default false).
+- `quantisationTable` / `quantizationTable`. Quantization table to use, integer 0-8 (optional, default 0).
+
+### png
+
+- `progressive`. Use progressive (interlace) scan (optional, default false)
+- `compressionLevel`. zlib compression level, 0-9 (optional, default 9).
+- `adaptiveFiltering`. Use adaptive row filtering (optional, default false).
+- `palette`. Quantise to a palette-based image with alpha transparency support (optional, default false).
+- `quality`. Use the lowest number of colours needed to achieve given quality, sets palette to true (optional, default 100).
+- `colours` / `colors`. maximum number of palette entries, sets palette to true (optional, default 256)
+- `dither`. Level of Floyd-Steinberg error diffusion, sets palette to true (optional, default 1.0)
+
+### webp
+
+- `quality`. Integer 1-100 (optional, default 80).
+- `alphaQuality`. Quality of alpha layer, integer 0-100 (optional, default 100).
+- `lossless`. Use lossless compression mode (optional, default false).
+- `nearLossless`. Use near_lossless compression mode (optional, default false).
+- `smartSubsample`. Use high quality chroma subsampling (optional, default false).
+- `reductionEffort`. Level of CPU effort to reduce file size, integer 0-6 (optional, default 4).
+- `pageHeight`. Page height for animated output.
+- `loop`. Number of animation iterations, use 0 for infinite animation (optional, default 0).
+- `delay`. list of delays between animation frames (in milliseconds).
+  
+  ```
+  /cat.png?ext=webp&options[delay][]=100&options[delay][]=100
+  ```
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const sharp = require('sharp');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('common'));
 app.use(function (req, res) {
   const ext = req.query.ext;
   const filePath = path.join(__dirname, 'public', req.path);
@@ -17,7 +19,47 @@ app.use(function (req, res) {
   height = (typeof height == 'number' && height > 0) ? height : null;
   
   if (ext && supportedOutputFormats.includes(ext)) {
-    sharpi = sharpi.toFormat(ext);
+    const options = req.query.options;
+
+    if (options) {
+      // common
+      options.quality && (options.quality = parseInt(options.quality));
+      options.progressive && (options.progressive = options.progressive == 'true' ? true : false);
+
+      // jpeg
+      options.optimiseCoding && (options.optimiseCoding = options.optimiseCoding == 'true' ? true : false);
+      options.optimizeCoding && (options.optimizeCoding = options.optimizeCoding == 'true' ? true : false);
+      options.trellisQuantisation && (options.trellisQuantisation = options.trellisQuantisation == 'true' ? true : false);
+      options.overshootDeringing && (options.overshootDeringing = options.overshootDeringing == 'true' ? true : false);
+      options.optimiseScans && (options.optimiseScans = options.optimiseScans == 'true' ? true : false);
+      options.optimizeScans && (options.optimizeScans = options.optimizeScans == 'true' ? true : false);
+      options.quantisationTable && (options.quantisationTable = parseInt(options.quantisationTable));
+      options.quantizationTable && (options.quantizationTable = parseInt(options.quantizationTable));
+
+      // png
+      options.compressionLevel && (options.compressionLevel = parseInt(options.compressionLevel));
+      options.adaptiveFiltering && (options.adaptiveFiltering = options.adaptiveFiltering == 'true' ? true : false);
+      options.palette && (options.palette = options.palette == 'true' ? true : false);
+      options.colors && (options.colors = parseInt(options.colors));
+      options.colours && (options.colours = parseInt(options.colours));
+      options.dither && (options.dither = parseFloat(options.dither));
+      
+      // webp
+      options.alphaQuality && (options.alphaQuality = parseInt(options.alphaQuality));
+      options.lossless && (options.lossless = options.lossless == 'true' ? true : false);
+      options.nearLossless && (options.nearLossless = options.nearLossless == 'true' ? true : false);
+      options.smartSubsample && (options.smartSubsample = options.smartSubsample == 'true' ? true : false);
+      options.reductionEffort && (options.reductionEffort = parseInt(options.reductionEffort));
+      options.pageHeight && (options.pageHeight = parseInt(options.pageHeight));
+      options.loop && (options.loop = parseInt(options.loop));
+
+      if (options.delay && Array.isArray(options.delay)) {
+        options.delay = options.delay.map(delay => parseInt(delay));
+      }
+    }
+
+    console.debug('options', options);
+    sharpi = sharpi.toFormat(ext, options);
   }
 
   sharpi.resize(width, height, {fit: (width && height) ? 'fill' : 'cover'}).toBuffer(function (err, data, info) {
